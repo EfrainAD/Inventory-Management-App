d15417fc4729e9f19dead007375d57fd36719c63
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-01-03 00:02:16 -0500

    Setup Routing to the home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/home";
+
 function App() {
   return (
-    <>
-      hello world
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
