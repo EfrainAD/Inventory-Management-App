6dc198037fbfc14910777c5b3c8f9795f7a12044
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-01-03 13:35:58 -0500

    react auth pages tmeplate and routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Forgot from "./pages/auth/forgot";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
+import Reset from "./pages/auth/reset";
 import Home from "./pages/home/home";
 
 function App() {
@@ -6,6 +10,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot" element={<Forgot />} />
+        <Route path="/resetpassword/:resetToken" element={<Reset />} />
       </Routes>
     </BrowserRouter>
   );
