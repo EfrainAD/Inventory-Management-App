1ec663196cf09dd7de31884c11d95e4f137914b7
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-01-03 15:28:18 -0500

    made navbar and layout for the dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/layout";
+import Sidebar from "./components/sidebar/sidebar";
 import Forgot from "./pages/auth/forgot";
 import Login from "./pages/auth/login";
 import Register from "./pages/auth/register";
 import Reset from "./pages/auth/reset";
+import Dashboard from "./pages/dashboard/dashboard";
 import Home from "./pages/home/home";
 
 function App() {
@@ -14,6 +17,14 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/forgot" element={<Forgot />} />
         <Route path="/resetpassword/:resetToken" element={<Reset />} />
+        
+        <Route path="/dashboard" element={
+          <Sidebar>
+            <Layout>
+              <Dashboard />
+            </Layout>
+          </Sidebar>
+        } />
       </Routes>
     </BrowserRouter>
   );
