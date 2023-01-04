00af9c5706fcf7c0c893f2336d49b6b739753f4e
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-01-03 20:08:13 -0500

    register now functions

--- src/pages/auth/register.js
+++ src/pages/auth/register.js
@@ -1,47 +1,102 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React, { useState } from "react"
+import { Link, Navigate, useNavigate } from "react-router-dom"
 import styles from "./auth.module.scss"
 import { TiUserAddOutline } from "react-icons/ti"
+import { toast } from "react-toastify"
 import Card from "../../components/card/card"
+import { registerUser, validateEmail } from "../../service/authService"
+import { SET_LOGIN, SET_NAME } from "../../redux/features/auth/authSlice"
+import {useDispatch} from 'react-redux'
+import Loader from "../../components/loader/loader"
+
+const initialState = {
+     name: '',
+     email: '',
+     password: '',
+     comfirm_password: '',
+}
 
 const Register = () => {
-  return (
-    <div className={`container ${styles.auth}`}>
-      <Card>
-        <div className={styles.form}>
-          <div className="--flex-center">
-            <TiUserAddOutline size={35} color="#999" />
-          </div>
-          <h2>Register</h2>
+     const dispatch = useDispatch()
+     const navigate = useNavigate()
+     const [isLoading, setIsLoading] = useState(false)
+     const [formData, setFormData] = useState(initialState)
+     const {name, email, password, comfirm_password} = formData
 
-          <form>
-            <input type="text" placeholder="Name" required name="name" />
-            <input type="email" placeholder="Email" required name="email" />
-            <input
-              type="password"
-              placeholder="Password"
-              required
-              name="password"
-            />
-            <input
-              type="password"
-              placeholder="Confirm Password"
-              required
-              name="password"
-            />
-            <button type="submit" className="--btn --btn-primary --btn-block">
-              Register
-            </button>
-          </form>
+     const handleInputChange = (e) => {
+          const {name, value} = e.target
+          setFormData({...formData, [name]: value})
+     }
+     const register = async (e) => {
+          e.preventDefault()
+          // Validation
+          if (!name, !email, !password, !comfirm_password)
+               return toast.error('All fields are required')
+          if (password !== comfirm_password)
+               return toast.error('Passwords do not match')
+          if (password.length < 8)
+               return toast.error('passowrd must be 8 characters long')
+          if (!validateEmail(email))
+               return toast.error('Email not valid')
+          const userData = {
+               name,
+               email,
+               password
+          }
+          setIsLoading(true)
+          try {
+               const data = await registerUser(userData)
+               await dispatch(SET_LOGIN(true))
+               await dispatch(SET_NAME(data.name))
+               navigate('/dashboard')
+               setIsLoading(false)
+          } catch (error) {
+               setIsLoading(false)
+               console.log(error.message)
+          }
+     }
+     return (
+     <div className={`container ${styles.auth}`}>
+          {isLoading && <Loader />}
+          <Card>
+               <div className={styles.form}>
+                    <div className="--flex-center">
+                    <TiUserAddOutline size={35} color="#999" />
+                    </div>
+                    <h2>Register</h2>
 
-          <span className={styles.register}>
-            <Link to="/">Home</Link>
-            <p> &nbsp; Already have an account? &nbsp;</p>
-            <Link to="/login">Login</Link>
-          </span>
-        </div>
-      </Card>
-    </div>
+                    <form onSubmit={register}>
+                         <input type="text" placeholder="Name" required name="name" value={name} onChange={handleInputChange} />
+                         <input type="email" placeholder="Email" required name="email" value={email} onChange={handleInputChange} />
+                         <input
+                              type="password"
+                              placeholder="Password"
+                              required
+                              name="password"
+                              value={password}
+                              onChange={handleInputChange}
+                         />
+                         <input
+                              type="password"
+                              placeholder="Confirm Password"
+                              required
+                              name="comfirm_password"
+                              value={comfirm_password}
+                              onChange={handleInputChange}
+                         />
+                         <button type="submit" className="--btn --btn-primary --btn-block">
+                         Register
+                         </button>
+                    </form>
+
+                    <span className={styles.register}>
+                    <Link to="/">Home</Link>
+                    <p> &nbsp; Already have an account? &nbsp;</p>
+                    <Link to="/login">Login</Link>
+                    </span>
+               </div>
+          </Card>
+     </div>
   )
 }
 
--- src/service/authService.js
+++ src/service/authService.js
@@ -1,19 +1,26 @@
 import axios from 'axios'
-import toast from 'react-toastify'
-export const API_URL = process.env.REACT_BACKEND_URL
+import { toast } from "react-toastify"
 
-export const refisterUser = async (userData) => {
+export const API_URL = process.env.REACT_APP_BACKEND_URL
+
+export const validateEmail = (email) => {
+     const emailValidater = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
+
+     return email.match(emailValidater)
+}
+
+export const registerUser = async (userData) => {
      try {
-          const response = await axios.post(`${API_URL}/users/register`, userData)
-          if (response.statusText === 'ok') {
+          const response = await axios.post(`${API_URL}/api/users/register`, userData)
+          
+          if (response.statusText === 'Created') {
                toast.success('User Registered successfully')
                return response.data
           }
      } catch (error) {
-          const errMsg = 
-               (error.response && error.response.data && error.response.msg) 
-               || error.msg 
-               || error.toString()
-          toast.error(errMsg)
+          const errorMessage = (error.response && error.response.data && error.response.msg) 
+          || error.message 
+          || error.toString()
+          toast.error(errorMessage)
      }
 }
