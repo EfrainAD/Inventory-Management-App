45de6d2f4f1f6184b4c76c023e49277a25453995
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-01-15 00:24:49 -0500

    Added functionality to the add product form

---
+++ src/components/productForm/ProductForm.js
@@ -0,0 +1,12 @@
+import './productForm.scss'
+
+const ProductForm = ({ title, product, productImage, imagePreview, description, handleInputChange, handleImageChange, saveProduct}) => {
+     return (
+     <>
+          <h3 className='-mt'>{title}</h3>
+
+     </>
+     )
+}
+
+export default ProductForm
