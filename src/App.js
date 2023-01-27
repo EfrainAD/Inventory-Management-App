a2b2ca548970bdfbb967186858742341ea92a4fa
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-01-26 19:47:13 -0500

    added Contact Us route

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import ProductDetail from "./components/product/productDetail/ProductDetail";
 import EditProduct from "./pages/editProject/EditProduct";
 import Profile from "./pages/profile/Profile";
 import EditProfile from "./pages/profile/EditProfile";
+import Contact from "./pages/contact/Contact";
 
 axios.defaults.withCredentials = true
 
@@ -89,6 +90,13 @@ function App() {
           <Sidebar>
             <Layout>
               <EditProfile />
+            </Layout>
+          </Sidebar>
+        } />
+        <Route path="dashboard/contact-us" element={
+          <Sidebar>
+            <Layout>
+              <Contact />
             </Layout>
           </Sidebar>
         } />
