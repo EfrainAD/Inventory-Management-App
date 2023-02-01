7e5e12ba20f06e8c943c91de97f8e125e742d53e
EfrainAD <105945226+EfrainAD@users.noreply.github.com>
2023-02-01 15:57:53 -0500

    1. I changes that when the user's name is not provided, it would fetch for it.
    Notes: The tutorial way to do this was to upload the name to the web browser’s local storage. That way when the user refreshed the page, (wiping out redux) the user’s name would still be there. This had an issue, and I was not sure how I felt about that method.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,31 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
-import { selectName, SET_LOGIN } from "../../redux/features/auth/authSlice"
-import { signOutUser } from "../../service/authService"
+import { selectName, SET_LOGIN, SET_NAME, SET_USER } from "../../redux/features/auth/authSlice"
+import { getUserProfile, signOutUser } from "../../service/authService"
 
 const Header = () => {
      const dispatch = useDispatch()
      const navigate = useNavigate()
      const name = useSelector(selectName)
 
+     // If the user's display name is not give, then fetch it.
+     useEffect(() => {
+          const runAsync = async () => {
+               if (name === '') {
+                    const user = await getUserProfile()
+                    if (user) {
+                         dispatch(SET_USER(user))
+                         dispatch(SET_NAME(user.name))
+                    }
+               }
+          }
+          runAsync()
+     }, [dispatch, name])
+
      const handleSignOut = async () => {
           await signOutUser()
-          await dispatch(SET_LOGIN(false))
+          dispatch(SET_LOGIN(false))
           navigate('/login')
      }
      return (
