f7add6e790c5306d3aaa5a73144e79ec74c0fbd1
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-02-07 16:49:44 -0500

    Changed this so the name would always update when the user refresh the page.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,27 +1,28 @@
-import React, { useEffect } from "react"
+import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
-import { selectName, SET_LOGIN, SET_NAME, SET_USER } from "../../redux/features/auth/authSlice"
+import { selectIsLoggedIn, selectName, SET_LOGIN, SET_NAME, SET_USER } from "../../redux/features/auth/authSlice"
 import { getUserProfile, signOutUser } from "../../service/authService"
 
 const Header = () => {
      const dispatch = useDispatch()
      const navigate = useNavigate()
      const name = useSelector(selectName)
+     const isLoggedIn = useSelector(selectIsLoggedIn)
 
-     // If the user's display name is not give, then fetch it.
      useEffect(() => {
           const runAsync = async () => {
-               if (name === '') {
-                    const user = await getUserProfile()
-                    if (user) {
-                         dispatch(SET_USER(user))
-                         dispatch(SET_NAME(user.name))
-                    }
+               if (!isLoggedIn) return
+               
+               const user = await getUserProfile()
+               if (user) {
+                    dispatch(SET_USER(user))
+                    dispatch(SET_NAME(user.name))
                }
           }
+
           runAsync()
-     }, [dispatch, name])
+     }, [dispatch, name, isLoggedIn])
 
      const handleSignOut = async () => {
           await signOutUser()
