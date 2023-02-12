d10d6880c16d546c2a54f031121cf311ad7a8d84
Efrain A. Davila <105945226+EfrainAD@users.noreply.github.com>
2023-02-12 16:26:17 -0500

    Change show the image is displayed in the image page. Now displays nicely with differnt size images, and is centered.

--- src/components/product/productDetail/ProductDetail.js
+++ src/components/product/productDetail/ProductDetail.js
@@ -35,64 +35,64 @@ const ProductDetail = () => {
           <div className="product-detail">
           <h3 className="--mt">Product Detail</h3>
           <Card cardClass="card">
-          {isLoading && <SpinningImg />}
-          {product && (
-               <div className="detail">
-                    {/* Image */}
-                    <Card cardClass="group">
-                         {product?.image ? (
-                              <img
-                                   src={product.image.filePath}
-                                   alt={product.image.fileName}
-                              />
-                         ) : (
-                              <p>No image set for this product</p>
-                         )}
-                    </Card>
-                    {/* Stock Status */}
-                    <h4>
-                         Product Availability: {stockStatus(product.quantity)}
-                    </h4>
-                    <hr />
-                    {/* Name */}
-                    <h4>
-                         <span className="badge">Name: </span> &nbsp; {product.name}
-                    </h4>
-                    {/* SKU */}
-                    <p>
-                         <b>&rarr; SKU : </b> {product.sku}
-                    </p>
-                    {/* Category */}
-                    <p>
-                         <b>&rarr; Category : </b> {product.category}
-                    </p>
-                    {/* Price */}
-                    <p>
-                         <b>&rarr; Price : </b> {moneyFormat(product.price)}
-                    </p>
-                    {/* Quantity */}
-                    <p>
-                         <b>&rarr; Quantity in stock : </b> {product.quantity}
-                    </p>
-                    {/* Total Stock Value */}
-                    <p>
-                         <b>&rarr; Total Value in stock : </b> {moneyFormat(product.price * product.quantity)}
-                    </p>
-                    <hr />
-                    <div dangerouslySetInnerHTML={{
-                         __html: DOMPurify.sanitize(product.description),
-                    }}
-                    ></div>
-                    <hr />
-                    <code className="--color-dark">
-                         Created on: {product.createdAt.toLocaleString("en-US")}
-                    </code>
-                    <br />
-                    <code className="--color-dark">
-                         Last Updated: {product.updatedAt.toLocaleString("en-US")}
-                    </code>
-               </div>
-          )}
+               {isLoading && <SpinningImg />}
+               {product && (
+                    <div className="detail">
+                         {/* Image */}
+                         <Card cardClass="group">
+                              {product?.image ? (
+                                   <img
+                                        src={product.image.filePath}
+                                        alt={product.image.fileName}
+                                   />
+                              ) : (
+                                   <p>No image set for this product</p>
+                              )}
+                         </Card>
+                         {/* Stock Status */}
+                         <h4>
+                              Product Availability: {stockStatus(product.quantity)}
+                         </h4>
+                         <hr />
+                         {/* Name */}
+                         <h4>
+                              <span className="badge">Name: </span> &nbsp; {product.name}
+                         </h4>
+                         {/* SKU */}
+                         <p>
+                              <b>&rarr; SKU : </b> {product.sku}
+                         </p>
+                         {/* Category */}
+                         <p>
+                              <b>&rarr; Category : </b> {product.category}
+                         </p>
+                         {/* Price */}
+                         <p>
+                              <b>&rarr; Price : </b> {moneyFormat(product.price)}
+                         </p>
+                         {/* Quantity */}
+                         <p>
+                              <b>&rarr; Quantity in stock : </b> {product.quantity}
+                         </p>
+                         {/* Total Stock Value */}
+                         <p>
+                              <b>&rarr; Total Value in stock : </b> {moneyFormat(product.price * product.quantity)}
+                         </p>
+                         <hr />
+                         <div dangerouslySetInnerHTML={{
+                              __html: DOMPurify.sanitize(product.description),
+                         }}
+                         ></div>
+                         <hr />
+                         <code className="--color-dark">
+                              Created on: {product.createdAt.toLocaleString("en-US")}
+                         </code>
+                         <br />
+                         <code className="--color-dark">
+                              Last Updated: {product.updatedAt.toLocaleString("en-US")}
+                         </code>
+                    </div>
+               )}
           </Card>
         </div>
   )
